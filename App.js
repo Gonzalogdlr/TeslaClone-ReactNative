00bbb49b7e3c6201ea369c81658730712e29341f
gonzalo <gologdlr@gmail.com>
2021-03-23 03:06:17 -0500

    Add The component CarItem in Appjs

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
-
+import CarItem from "./assets/components/CarItem/Index";
 export default function App() {
   return (
     <View style={styles.container}>
-      
-
+      <CarItem/>
       <StatusBar style="auto" />
     </View>
   );
